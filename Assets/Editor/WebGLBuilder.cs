01c8c48b6f1b24926ce87c0c9e6f92d3010f4d85
Julius Herzig <jh1521@ic.ac.uk>
2022-02-07 16:41:35 +0000

    Fixed scene name.

--- Assets/Editor/WebGLBuilder.cs
+++ Assets/Editor/WebGLBuilder.cs
@@ -7,7 +7,7 @@ using UnityEditor;
 
 class WebGLBuilder {
     static void build() {
-        string[] scenes = {"Assets/Scenes/MainScene.unity"};
+        string[] scenes = {"Assets/Scenes/SampleScene.unity"};
         BuildPipeline.BuildPlayer(scenes, "WebGL-Dist", BuildTarget.WebGL, BuildOptions.None);
     }
 }
