55d0cac0f93095a3e6e0bcc58831d0faf4f3fb8d
Nikolai Merritt <nikolaimerritt20@gmail.com>
2022-02-08 15:28:55 +0000

    added debug.logs

--- Assets/DatabaseRunner.cs
+++ Assets/DatabaseRunner.cs
@@ -9,14 +9,22 @@ public class DatabaseRunner : MonoBehaviour
     public DatabaseViewTest database;
     void Start() 
     {
+        Debug.Log("Entered Start() function!");
+        Debug.Log("Creating Database class...");
+
         database = new DatabaseViewTest("neo4j://cloud-vm-42-36.doc.ic.ac.uk:7687", "neo4j", "s3cr3t");
+        Debug.Log("Database class created");
+
         List<string> readTitles = database.ReadAllNodeTitles();
         foreach (string title in readTitles)
-            Debug.Log($"Found title {title}");
+            Debug.Log($"Read from database, found title {title}");
 
         string datetime = DateTime.Now.ToString("dd/MM/yyyy  hh:mm:ss");
         string toWrite = $"Created new rubbish node on {datetime}";
+        
         Debug.Log($"Writing some rubbish: \n{toWrite}");
         database.WriteRubbish(toWrite);
+
+        Debug.Log("Written the rubbish");
     }
 }
